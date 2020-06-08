7f4f0fb9a1714488aac494ccef3ed7e479ef7910
kuCasino <luunguyenduc1995@gmail.com>
2020-06-08 20:28:05 +0700

    code-chart

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '13.0'
+
+target 'Practice_Chart' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Practice_Chart
+  pod 'Charts'
+  pod 'TinyConstraints'
+
+end
--- Practice_Chart/ViewController.swift
+++ Practice_Chart/ViewController.swift
@@ -7,13 +7,98 @@
 //
 
 import UIKit
+import Charts
+import TinyConstraints
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ChartViewDelegate {
+    
+    lazy var lineChartView: LineChartView = {
+        let chartView = LineChartView()
+        chartView.backgroundColor = .systemGreen
+        
+        chartView.rightAxis.enabled = false
+        
+        let yAxis = chartView.leftAxis
+        yAxis.labelFont = .boldSystemFont(ofSize: 12)
+        yAxis.setLabelCount(5, force: false)
+        yAxis.labelTextColor = .white
+        yAxis.axisLineColor = .white
+        yAxis.axisLineWidth = 2
+        yAxis.labelPosition = .outsideChart
+        
+        chartView.xAxis.axisLineWidth = 2
+        chartView.xAxis.axisLineColor = .white
+        chartView.xAxis.labelTextColor = .white
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.labelFont = .boldSystemFont(ofSize: 12)
+        
+        return chartView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(lineChartView)
+        lineChartView.centerInSuperview()
+        lineChartView.width(to: view)
+        lineChartView.height(view.frame.width/2)
+        
+        setData()
     }
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
+    }
+    
+    func setData() {
+        let lineData = LineChartDataSet(entries: yValues, label: "DOANH THU")
+        
+        lineData.mode = .cubicBezier
+        lineData.drawCirclesEnabled = false
+        lineData.lineWidth = 4
+        lineData.setColor(.brown)
+        lineData.fill = Fill(color: .brown)
+        lineData.fillAlpha = 0.5
+        lineData.drawFilledEnabled = true
+        lineData.drawHorizontalHighlightIndicatorEnabled = false
+        lineData.highlightColor = .white
+        
+        let data = LineChartData(dataSet: lineData)
+        
+        data.setDrawValues(false)
+        
+        lineChartView.data = data
+    }
+    
+    let yValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 0.0, y: 10.0),
+        ChartDataEntry(x: 1.0, y: 5.0),
+        ChartDataEntry(x: 2.0, y: 7.0),
+        ChartDataEntry(x: 3.0, y: 5.0),
+        ChartDataEntry(x: 4.0, y: 10.0),
+        ChartDataEntry(x: 5.0, y: 6.0),
+        ChartDataEntry(x: 6.0, y: 5.0),
+        ChartDataEntry(x: 7.0, y: 7.0),
+        ChartDataEntry(x: 8.0, y: 8.0),
+        ChartDataEntry(x: 9.0, y: 12.0),
+        ChartDataEntry(x: 10.0, y: 13.0),
+        ChartDataEntry(x: 11.0, y: 5.0),
+        ChartDataEntry(x: 12.0, y: 7.0),
+        ChartDataEntry(x: 13.0, y: 3.0),
+        ChartDataEntry(x: 14.0, y: 15.0),
+        ChartDataEntry(x: 15.0, y: 6.0),
+        ChartDataEntry(x: 16.0, y: 6.0),
+        ChartDataEntry(x: 17.0, y: 7.0),
+        ChartDataEntry(x: 18.0, y: 3.0),
+        ChartDataEntry(x: 19.0, y: 10.0),
+        ChartDataEntry(x: 20.0, y: 12.0),
+        ChartDataEntry(x: 21.0, y: 15.0),
+        ChartDataEntry(x: 22.0, y: 17.0),
+        ChartDataEntry(x: 23.0, y: 15.0),
+        ChartDataEntry(x: 24.0, y: 10.0),
+        ChartDataEntry(x: 25.0, y: 10.0),
+        ChartDataEntry(x: 26.0, y: 10.0),
+        ChartDataEntry(x: 27.0, y: 17.0),
+    ]
 
 
 }
